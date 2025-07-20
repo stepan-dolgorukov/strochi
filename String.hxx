266b20a4c1de91fdf5d4802745fc2def5c54c8a1
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2025-07-20 13:43:55 +0500

    Declare operators in type "String"

--- String.hxx
+++ String.hxx
@@ -7,4 +7,10 @@ class String
     String(const char* pointerToCString);
     String(const String& referenceToString);
     String(String&& referenceToString);
+    String& operator=(const char* pointerToCString);
+    String& operator=(const String& referenceToString);
+    String& operator=(String&& referenceToString);
+    String& operator+=(const char* pointerToCString);
+    String& operator+=(const String& referenceToString);
+    String& operator=(String&& referenceToString);
 };
