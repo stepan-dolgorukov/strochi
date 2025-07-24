a851f4794782514f4f2724889b1b05a961bd7564
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2025-07-24 21:30:33 +0500

    Define a program to sort strings in ">" relation

--- Sort.cxx
+++ Sort.cxx
@@ -1,4 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "String.hxx"
+
 int main()
 {
+    std::vector<String> strings{};
+
+    for (String string{}; std::cin >> string; )
+    {
+        strings.push_back(std::move(string));
+    }
+
+    std::sort(strings.begin(), strings.end(), std::greater<String>{});
+
+    for (const String& referenceToString : strings)
+    {
+        std::cout << referenceToString << '\n';
+    }
+
     return 0;
 }
